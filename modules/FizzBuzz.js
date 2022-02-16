f9c2ad58d0ae6c61559ec46a8d1b73976d2285a3
Kevin Sivic <kevin.sivic@gmail.com>
2022-02-16 14:54:33 -0500

    Initial FizzBuzz

---
+++ modules/FizzBuzz.js
@@ -0,0 +1,5 @@
+function evaluate(number) {
+    return undefined;
+}
+
+module.exports = evaluate
---
+++ modules/tests/FizzBuzz.test.js
@@ -0,0 +1,5 @@
+const evaluate = require("../FizzBuzz");
+
+test('The answer to life the universe and everything', () => {
+    expect(evaluate([1])).toEqual(['1']);
+});
